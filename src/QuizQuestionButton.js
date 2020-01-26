cc450a37ba17f09fbd1eb76ee99438b1bd8790f5
noecl1 <noecl1@hotmail.com>
2020-01-26 00:51:10 -0700

    Creating a QuizQuestionButton Component

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+      <li>
+        <button>Hello World</button>
+      </li>
+    )
+  }
+}
+
+export default QuizQuestionButton
