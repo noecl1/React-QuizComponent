4c30c2b8b400e13c3a0c4abcf2c4c13c1bba3d4e
noecl1 <noecl1@hotmail.com>
2020-01-26 00:54:19 -0700

    fixed button_text

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button>Hello World</button>
+  <button>{this.props.button_text}</button>
       </li>
     )
   }
